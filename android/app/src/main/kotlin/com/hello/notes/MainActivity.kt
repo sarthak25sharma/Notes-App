bdcdb7917e332cab8c531ef6a833c14586125d2b
Sarthak Sharma <sarthak213@gmail.com>
2024-10-31 13:26:43 +0530

    login register routing

---
+++ android/app/src/main/kotlin/com/hello/notes/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.hello.notes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:notes/views/login_view.dart';
+import 'package:notes/views/register_view.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+      routes: {
+        '/login' :(context) => const LoginView(),
+        '/register': (context) => const RegisterView(),
+      },
+    ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState){
+            case ConnectionState.done:
+              // final user=FirebaseAuth.instance.currentUser;
+              // final emailVerfied=user?.emailVerified ?? false;
+              // if(emailVerfied){
+              //     //.
+              //     return const Text('Done');
+              // }
+              // else{
+              //   return const VerifyEmailView();
+              // }
+              return const LoginView();
+              
+            default:
+              return const CircularProgressIndicator();
+          }
+        },
+      );
+  }
+}
+
